1762cb2775c82e46801438049c7cf45f27c79051
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-01-31 15:07:24 -0300

    menu básico

--- 05_projetos/sabor_express/app.py
+++ 05_projetos/sabor_express/app.py
@@ -3,4 +3,6 @@ print('\nSabor Express\n')
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restautante')
-print('4. Sair')
+print('4. Sair\n')
+
+opcao_escolhida = int(input('Escolha uma opção: 1'))
