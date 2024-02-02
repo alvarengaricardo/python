f748c1df7963c3336667a8b605ee75e31c08b564
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-02-02 15:54:22 -0300

    lógica do menu

--- 05_projetos/sabor_express/app.py
+++ 05_projetos/sabor_express/app.py
@@ -8,8 +8,17 @@ print("""
 """)
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar restautante')
 print('4. Sair\n')
 
 opcao_escolhida = int(input('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
