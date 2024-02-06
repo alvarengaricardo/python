95aa915acd2e725c4d632232b6793039228b8cbf
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2024-02-06 16:52:37 -0300

    implementação de 'try' no menu principal

--- 05_projetos/sabor_express/app.py
+++ 05_projetos/sabor_express/app.py
@@ -7,6 +7,11 @@ Desenvolvimento de um App para restaurantes
 
 import os
 
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar ao menu principal')
+    main()
+
 
 def finalizar_app():
     os.system('cls')
@@ -30,21 +35,25 @@ def escolher_opcoes():
     print('3. Ativar restautante')
     print('4. Sair\n')
 
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    match opcao_escolhida:
-        case 1:
-            print('Adicionar restaurante')
-        case 2:
-            print('Listar restaurantes')
-        case 3:
-            print('Ativar restaurante')
-        case 4:
-            finalizar_app()
-        case _:
-            print('Opção inválida!')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        match opcao_escolhida:
+            case 1:
+                print('Adicionar restaurante')
+            case 2:
+                print('Listar restaurantes')
+            case 3:
+                print('Ativar restaurante')
+            case 4:
+                finalizar_app()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 
 def main():
+    os.system('cls')
     exibir_nome()
     escolher_opcoes()
 
